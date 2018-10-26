ecbb8e29e08865114c824f54bc0b9bbada8a4f9a
SilvioBonfi <silvio.biasiol@bonfiglioli.com>
2018-10-26 16:13:45 +0200

    small sizing fixes

--- src/components/productsCards/ProductsCards.js
+++ src/components/productsCards/ProductsCards.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import './products-cards.css';
-
+// Boostrap
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button,
     Row, Col, Container
 } from 'reactstrap';
+// Icons
+import { FaShoppingCart } from 'react-icons/fa';
 
 const Product = () => (
     <div>
-        <Card className='product-card'>
-            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+        <Card className='product-card text-center'>
+            <CardImg className="product-card-image" top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
             <CardBody>
                 <CardTitle>Card title</CardTitle>
                 <CardSubtitle>Card subtitle</CardSubtitle>
+                <a className="buy"><FaShoppingCart /></a>
                 <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
                 <Button>Button</Button>
             </CardBody>
@@ -24,9 +27,9 @@ const Product = () => (
 const ProductsCards = () => (
     <Container fluid id='products-cards' className="d-flex justify-content-center">
         <Row>
-            <Col fluid ><Product /></Col>
-            <Col fluid ><Product /></Col>
-            <Col fluid><Product /></Col>
+            <Col className="d-flex justify-content-center" fluid ><Product /></Col>
+            <Col className="d-flex justify-content-center" fluid ><Product /></Col>
+            <Col className="d-flex justify-content-center" fluid><Product /></Col>
         </Row>
     </Container>
 )
